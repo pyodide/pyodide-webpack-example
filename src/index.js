c1094e5a0ba5775709833f25136e9d0b4e6cced3
grimmer <grimmer0125@gmail.com>
2021-11-04 00:47:26 +0800

    Add a demo code to show generated div text from Pyodide

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
-import _ from 'lodash';
-
 async function loadPyodide() {
-    let pyodide_pkg = await import("pyodide/pyodide.js");
-    let pyodide = await pyodide_pkg.loadPyodide({
-        indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/",
-    });
+  const pyodide_pkg = await import("pyodide/pyodide.js");
+  const pyodide = await pyodide_pkg.loadPyodide({
+    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/",
+  });
+
+  pyodide.runPython(`
+      import js
+      div = js.document.createElement("div")
+      div.innerHTML = "<h1>Hello Pyodide! This element was created from Python</h1>"
+      js.document.body.prepend(div)
+  `);
 }
 
-function component() {
-    loadPyodide();
-
-    const element = document.createElement('div');
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
+loadPyodide()
