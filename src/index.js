9156357ebc1402ffdccd7e37fb98bd44df31f982
Hood Chatham <roberthoodchatham@gmail.com>
2022-09-11 20:51:55 -0700

    Disable size warning

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ async function loadPyodide() {
   const pyodide_pkg = await import("pyodide/pyodide.js");
   const pyodide = await pyodide_pkg.loadPyodide();
   pyodide.runPython(`
-      import js
-      div = js.document.createElement("div")
-      div.innerHTML = "<h1>Hello Pyodide! This element was created from Python</h1>"
-      js.document.body.prepend(div)
+      print([x*x+1 for x in range(20)])
   `);
 }
 
